22e50e039923a2d5f503aeaf6d5ae20be1bb8e49
cumelmell <cumelmell@foxmail.com>
2022-01-27 10:41:28 +0800

    test
    Signed-off-by: cumelmell <cumelmell@foxmail.com>

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+import { add, subtract, multiply, division } from '../common/utils.js';
+
 console.log('business');
+
+console.log(add(1, 2));
+console.log(subtract(1, 2));
+console.log(multiply(1, 2));
+console.log(division(1, 2));
